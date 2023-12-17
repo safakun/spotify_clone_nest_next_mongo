59147f8b1b9cf369f433d9003d2f2bf87fbadd68
safakun <safarovdeem@gmail.com>
2023-12-17 11:53:18 +0500

    added mongo and schemas for track and comment

--- README.md
+++ README.md
@@ -6,4 +6,14 @@ nest new server
 
 ``` 
 
+Подключение к базе данных Mongo 
+https://docs.nestjs.com/techniques/mongodb
+
+```bash
+npm i @nestjs/mongoose mongoose
+```
+```bash
+sudo docker compose up
+
+```
 
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TrackModule } from "./track/track.module";
+import { MongooseModule } from "@nestjs/mongoose";
 
 
 @Module({
-  imports: [TrackModule]
+  imports: [TrackModule,
+    MongooseModule.forRoot(`mongodb://root:rootpassword@localhost:27017/test?authSource=admin`)
+  ]
 })
 export class AppModule {
 
