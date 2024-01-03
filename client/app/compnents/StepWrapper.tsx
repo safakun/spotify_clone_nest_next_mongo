98712971b7c7e91a94cd5d43550c9e167595ea22
safakun <safarovdeem@gmail.com>
2024-01-03 11:30:53 +0500

    added steps

---
+++ client/app/compnents/StepWrapper.tsx
@@ -0,0 +1,35 @@
+import { Container, Step, StepLabel, Stepper, Grid, Card } from '@mui/material';
+import React from 'react'
+
+interface StepWrapperProps {
+    activeStep: number;
+    children: any;
+}
+
+const steps = ['Track info', 'Load track picture', 'Load track audio file']
+
+const StepWrapper: React.FC<StepWrapperProps> = ({ activeStep, children }) => {
+  return (
+    <Container>
+      <Stepper activeStep={activeStep}>
+        {steps.map((step, index) => 
+            <Step
+            key={index}
+            >
+                <StepLabel>
+                    {step}
+                </StepLabel>
+                {/* {children} */}
+            </Step>
+            )}
+      </Stepper>
+      <Grid container justifyContent='center' className='steps'>
+        <Card className='stepscard'>
+            {children}
+        </Card>
+      </Grid>
+    </Container>
+  )
+}
+
+export default StepWrapper
--- client/app/tracks/create/page.tsx
+++ client/app/tracks/create/page.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import Mainlayout from '../../layouts/MainLayout'
+import StepWrapper from '@/app/compnents/StepWrapper'
+import { Button, Grid } from '@mui/material'
 
 const create = () => {
+
+  const [activeStep, setActiveStep] = useState(0)
+
+  const back = () => {
+    setActiveStep(prev => prev - 1)
+  }
+  const next = () => {
+    if (activeStep !== 3) {
+      setActiveStep(prev => prev + 1)
+    }
+    
+  }
+
   return (
     <Mainlayout>
-      <h1 className="center"> Загрузка трека</h1>
+      <StepWrapper activeStep={activeStep}>
+        { activeStep === 1 &&
+        <h1>STEP 1 </h1>
+        }    
+        { activeStep === 2 &&
+        <h1>STEP 2 </h1>
+        }   
+        { activeStep === 3 &&
+        <h1>STEP 3 </h1>
+        }   
+      </StepWrapper>
+     <Grid container justifyContent={'space-between'}>
+      <Button disabled={activeStep === 0} onClick={back}>Back</Button>
+      <Button onClick={next}>Next</Button>
+     </Grid>
      
     </Mainlayout>
   )
