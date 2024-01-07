1ced67d9fffbc021c469c26eb424ec82c06d8e6e
safakun <safarovdeem@gmail.com>
2024-01-07 08:12:32 +0500

    changed reducer

--- client/app/store/index.ts
+++ client/app/store/index.ts
@@ -1,8 +1,8 @@
 import { Context, MakeStore, createWrapper } from 'next-redux-wrapper'
-import { RootState, rootReducer } from './reducers';
+import { RootState, reducer } from './reducers';
 import { Store, createStore } from 'redux';
 // create a makeStore function
-const makeStore: MakeStore<Store<RootState>> = (context: Context) => createStore(rootReducer);
+const makeStore: MakeStore<Store<RootState>> = (context: Context) => createStore(reducer);
 
 // export an assembled wrapper
 export const wrapper = createWrapper<Store<RootState>>(makeStore, {debug: true});
